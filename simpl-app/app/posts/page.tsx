673ea53eab46bb10eeb5026539f346ac52b9b8ff
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-06-23 14:25:41 +0200

    list in posts

--- simpl-app/app/posts/page.tsx
+++ simpl-app/app/posts/page.tsx
@@ -8,15 +8,15 @@ export default async function Posts() {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+    <div className="">
+      <h1 className="">
         Posts
       </h1>
-      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
+      <ul className="">
         {posts.map((post) => (
           <li key={post.id}>
-            <span className="font-semibold text-gray-600">{post.title}</span>
-            <span className="text-sm text-gray-600 ml-2">
+            <span className="">{post.title}</span>
+            <span className="">
               by {post.author.name}
             </span>
           </li>
