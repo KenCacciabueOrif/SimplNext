24f6b95cd84cb792b839c1d1e9d1bdb608a2dd32
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-06-24 13:54:31 +0200

    service worker

--- simpl-app/app/page.tsx
+++ simpl-app/app/page.tsx
@@ -1,13 +1,17 @@
 
-import Posts from './posts/page';
 
-export default async function Home() {
+import Posts from './posts/page';
+import UESF from './_useEffectSW'
+
+
+export default function Home() {
   return (
     <div className="border h-screen items-center flex flex-col ">
       <h1 className="text-2xl font-bold border text-center w-full">
         Simpl.
       </h1>
       <ol className="border flex flex-col flex-auto w-full overflow-auto">
+        <UESF/>
         <Posts />
       </ol>
     </div>
