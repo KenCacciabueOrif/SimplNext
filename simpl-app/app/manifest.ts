321b56bdb3883b245fc729eee231f5d8a6d9fbe4
Ken Cacciabue <ken.cacciabue@formation.orif.ch>
2025-06-24 12:54:57 +0200

    manifest

--- simpl-app/app/manifest.ts
+++ simpl-app/app/manifest.ts
@@ -1,25 +1,53 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Simpl.',
     short_name: 'S.',
-    description: 'A simple social network',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#030712',
-    theme_color: '#030712',
     icons: [
       {
-        src: '/public/file.svg',
+        src: '/public/logo.svg',
         sizes: '192x192',
         type: 'image/psvg',
       },
       {
-        src: '/public/file.svg',
+        src: '/public/logo.svg',
         sizes: '512x512',
         type: 'image/svg',
       },
     ],
+    start_url: '/',
+    display: 'standalone',
+    theme_color: '#030712',
+    background_color: '#030712',
+    scope: '/',
+    lang: 'en',
+    dir: 'auto',
+    orientation: 'any',
+    description: 'A simple social network',
+    screenshots: [
+      {
+        src: 'public/SimplHome.png',
+        sizes: '384x854',
+        type: 'image/png',
+        form_factor: 'narrow',
+        label: "Home screen showing main navigation and featured content"
+      },
+      {
+        src: 'public/SimplMode.png',
+        sizes: '384x854',
+        type: 'image/png',
+        form_factor: 'narrow',
+        label: "Moderation screen showing main navigation and content to moderate"
+      },
+      {
+        src: 'public/SimplComm.png',
+        sizes: '384x854',
+        type: 'image/png',
+        form_factor: 'narrow',
+        label: "Commentary screen showing main post and its comments"
+      }
+    ],
+    categories: ['social networking ', 'social']
   }
 }
